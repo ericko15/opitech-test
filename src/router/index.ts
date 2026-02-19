f0ca3074985cee77a6c7a4cef983ff600eba5957
eric manosalva <ramses.manosalva@gmail.com>
2026-02-19 17:13:29 -0500

    fix: switch to createWebHashHistory for router history management

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 const routes = [
   {
@@ -14,6 +14,6 @@ const routes = [
 ];
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
